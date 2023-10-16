8f4da8fee6f4a64061d7f971f32435bcc9d9ab77
fedegime <fgimenez@cuoma.com>
2023-10-16 13:13:21 -0300

    update api

--- js/app.js
+++ js/app.js
@@ -51,7 +51,7 @@ function consultarAPI(ciudad, pais ) {
 
     // leer la url  y agregar el API key
     const appId = 'd5890d1eea5972135c1461af25d989f2';
-    let url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
 
     Spinner();
 
